8bee663a64e3ae31d21fe4cfd885f18124ac8f48
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-14 17:18:48 +0300

    create auth, register and login components to assist in user authentication

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+import Auth from "./auth/Auth"
+
+function App() {
+  return (
+    <div className="App">
+     <Auth></Auth>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/auth/Auth.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./Login";
+import Register from "./Register";
+
+const Auth = props => (
+  <div>
+    <Register></Register>
+    <Login></Login>
+  </div>
+);
+
+export default Auth;
