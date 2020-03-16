a582d0539da4677b8c38e5bff0ccf3b32a628584
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-16 12:23:05 +0300

    configure whether to show the authentication page or home page depending on whether the user is authenticated

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import '../styles/App.css';
-import Auth from "./auth/Auth"
+import React from "react";
+import "../styles/App.css";
+import Auth from "./auth/Auth";
+import Navbar from "./navigation/NavigationBar";
+import PostList from "./PostList";
+
 
 function App() {
   return (
     <div className="App">
-     <Auth></Auth>
+      <Navbar />
+      <PostList/>
+  
     </div>
   );
 }
