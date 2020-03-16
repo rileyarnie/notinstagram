7234d031559705f9d0ab67742fc083e6776284b1
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-16 12:43:49 +0300

    create Post model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+import datetime
 # Create your models here.
+
+class Post(models.Model):
+    image=models.ImageField(null=False)
+    caption=models.TextField(null=False)
+    posted_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    date_posted=models.DateField(auto_now_add=True)
