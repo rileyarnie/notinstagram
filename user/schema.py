6dda8349351feb3077a556fcf94259b72d752b21
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-16 12:37:41 +0300

    define a me query to test whether user is authenticated or not

---
+++ user/schema.py
@@ -0,0 +1,47 @@
+import graphene
+from django.contrib.auth import get_user_model
+from graphene_django import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class Query(graphene.ObjectType):
+    me = graphene.Field(UserType)
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info):
+        return get_user_model().objects.all()
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Please Log In!!")
+
+        return user
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        first_name = graphene.String(required=True)
+        last_name = graphene.String(required=True)
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate(self, info, first_name, last_name, username, email, password):
+        user = get_user_model()(
+            first_name=first_name, last_name=last_name, username=username, email=email,
+        )
+        user.set_password(password)
+        user.save()
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
