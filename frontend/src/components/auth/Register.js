e4e6b762bdc5f91c756216469752fb386a764fe6
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-16 12:21:09 +0300

    create the register component and allow it to carry out a mutation to create a new user when submited

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,9 +1,152 @@
 import React, { Component } from "react";
+import { Form, Button, Container, Col } from "react-bootstrap";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
 class Register extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: ""
+  };
   render() {
-    return <div>Register</div>;
+    const { firstName, lastName, username, email, password } = this.state;
+
+    const handleSubmit = (event, createUser) => {
+      event.preventDefault();
+      createUser();
+    };
+    return (
+      <Container>
+        Register
+        <Mutation
+          mutation={REGISTER_MUTATION}
+          variables={{ firstName, lastName, username, email, password }}
+          onCompleted={data => {
+            console.log({ data });
+          }}
+        >
+          {(createUser, { loading, error }) => {
+            return (
+              <Form onSubmit={event => handleSubmit(event, createUser)}>
+                <Form.Group controlId="firstName">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    onChange={event =>
+                      this.setState({ firstName: event.target.value })
+                    }
+                    value={firstName}
+                    type=""
+                    placeholder="First Name"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="lastName">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    onChange={event =>
+                      this.setState({ lastName: event.target.value })
+                    }
+                    value={lastName}
+                    type=""
+                    placeholder="Last Name"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="username">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    onChange={event =>
+                      this.setState({ username: event.target.value })
+                    }
+                    value={username}
+                    type=""
+                    placeholder="Username"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    onChange={event =>
+                      this.setState({ email: event.target.value })
+                    }
+                    value={email}
+                    type="email"
+                    placeholder="Enter email"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    onChange={event =>
+                      this.setState({ password: event.target.value })
+                    }
+                    value={password}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Group>
+                <Form.Row>
+                  <Form.Group>
+                    <Col>
+                      <Button
+                        className=""
+                        variant="primary"
+                        type="submit"
+                        disabled={
+                          loading ||
+                          !username.trim() ||
+                          !email.trim() ||
+                          !password.trim() ||
+                          !firstName.trim() ||
+                          !lastName.trim()
+                        }
+                      >
+                        Submit
+                      </Button>
+                    </Col>
+                    <Col>
+                      <Form.Text className="text-muted" md="">
+                        Already Have an Account? Sign In.
+                      </Form.Text>
+                    </Col>
+                  </Form.Group>
+                </Form.Row>
+                {error && <div>Error!!</div>}
+              </Form>
+            );
+          }}
+        </Mutation>
+      </Container>
+    );
   }
 }
 
 export default Register;
+
+const REGISTER_MUTATION = gql`
+  mutation SignupMutation(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+    $username: String!
+  ) {
+    createUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+      username: $username
+    ) {
+      user {
+        username
+        email
+      }
+    }
+  }
+`;
