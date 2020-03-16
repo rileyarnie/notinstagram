e689f9bf24339659a89964cd26cd34c564311c19
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-16 12:19:56 +0300

    add the mutation for the jwt tokens

---
+++ app/schema.py
@@ -0,0 +1,16 @@
+import graphene
+import user.schema
+import graphql_jwt
+
+
+class Query(user.schema.Query, graphene.ObjectType):
+    pass
+
+
+class Mutation(user.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
