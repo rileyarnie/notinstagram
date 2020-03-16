aedd951b77cd2ec667236ebc00c0ffa0009dad70
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-16 12:36:20 +0300

    configure the react frontend to allow the stirage and state management depending on whether the jwt is available or not

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,54 @@ import "./styles/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { ApolloProvider, Query } from "react-apollo";
+import ApolloClient, { gql } from "apollo-boost";
+import PostList from "./components/PostList";
+import Auth from "./components/auth/Auth";
+import { Navbar } from "react-bootstrap";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql/",
+
+  request: operation => {
+    const token = localStorage.getItem("authToken") || "";
+    operation.setContext({
+      headers: {
+        Authorization: `JWT ${token} `
+      }
+    });
+  },
+
+  fetchOptions: {
+    credentials: "include"
+  },
+
+  clientState: {
+    defaults: {
+      isLoggedIn: !!localStorage.getItem("authToken")
+    }
+  },
+
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("graphQLErrors", graphQLErrors);
+    console.log("networkError", networkError);
+  }
+});
+
+const IS_LOGGEDIN_QUERY = gql`
+  query {
+    isLoggedIn @client
+  }
+`;
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Query query={IS_LOGGEDIN_QUERY}>
+      {({ data }) => (data.isLoggedIn ? <App /> : <Auth />)}
+    </Query>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
