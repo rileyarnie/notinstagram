edadf75c7cc25b110440690ce91863d385f6890f
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-16 17:38:17 +0300

    pdate the app scheme to include the create post mutation

--- app/schema.py
+++ app/schema.py
@@ -1,13 +1,14 @@
 import graphene
 import user.schema
 import graphql_jwt
+import insta.schema
 
 
-class Query(user.schema.Query, graphene.ObjectType):
+class Query(insta.schema.Query,user.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(user.schema.Mutation, graphene.ObjectType):
+class Mutation(insta.schema.Mutations,user.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
