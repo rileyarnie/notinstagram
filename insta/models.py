ceb2c038d8a53d90d7d8dc5a7e7bc8daa500adb5
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-17 14:10:01 +0300

    register the signals created

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 import datetime
+from django.contrib.auth.models import User
+import PIL
+from PIL import Image
 # Create your models here.
 
 class Post(models.Model):
@@ -8,3 +11,19 @@ class Post(models.Model):
     caption=models.TextField(null=False)
     posted_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     date_posted=models.DateField(auto_now_add=True)
+
+class Profile(models.Model):
+    pic = models.ImageField(default = 'default.jpg', upload_to ='profile_pics')
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f"{self.user}'s profile")
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        img = PIL.Image.open(self.pic.path)
+
+        if img.height > 300 or img.width >300:
+            output_size = (152,152)
+            img.thumbnail(output_size)
+            img.save(self.pic.path)
