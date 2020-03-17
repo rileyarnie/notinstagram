6ad12630dc23333487b7cbf495f0cbbab8b6d15e
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-17 15:52:31 +0300

    add user profile pics to each post

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -4,13 +4,13 @@ import "../styles/Post.css";
 class Post extends Component {
   render() {
     return (
-      <article  className="Post" ref="Post">
+      <article className="Post" ref="Post">
         <header>
           <div className="Post-user">
             <div className="Post-user-avatar">
               <img
-                src="https://www.superherohype.com/assets/uploads/2019/12/Wonder-Woman-1984-Diana-featured-1280x720.jpg"
-                alt="Chris"
+                src={`http://localhost:8000/media/${this.props.post.postedBy.profile.pic}`}
+                alt="photo"
               />
             </div>
             <div className="Post-user-nickname">
@@ -20,7 +20,7 @@ class Post extends Component {
         </header>
         <div className="Post-image">
           <div className="Post-image-bg">
-            <img 
+            <img
               alt="Icon Living"
               src={`http://localhost:8000/media/${this.props.post.image}`}
             />
