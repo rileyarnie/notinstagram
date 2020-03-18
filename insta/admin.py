853a831fc679008f4aba862c66fc910cd17e1c55
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-18 21:38:30 +0300

    register comment model

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Profile, Comment
+from .models import Post, Profile, Comment, Like
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Profile)
 admin.site.register(Comment)
+admin.site.register(Like)
