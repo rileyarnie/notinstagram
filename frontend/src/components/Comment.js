a3f4147a41fb32031b9538c82c042197d989c582
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-22 15:49:14 +0300

    add link to username

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Comment extends Component {
 
@@ -7,8 +8,7 @@ class Comment extends Component {
   render() {
     return (
       <div>
-        {this.props.comment.content} by <a>{this.props.comment.user.username}</a>
-        
+        {this.props.comment.content} by <Link to={`/profile/${this.props.comment.user.id}`} >{this.props.comment.user.username}</ Link>
       </div>
     );
   }
