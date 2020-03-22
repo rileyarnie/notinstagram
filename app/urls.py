2f4e0005b10521b55b41ad8f24384d24f2598847
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-22 15:43:47 +0300

    append static url to the url patterns

--- app/urls.py
+++ app/urls.py
@@ -24,4 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
