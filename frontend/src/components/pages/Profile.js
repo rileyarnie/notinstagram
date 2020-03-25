f5da9a1fc79434c9c399295cff1bb268f2995b65
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-26 00:13:53 +0300

    create user profile page component

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 
-class Profile extends Component{
+class Profile extends Component {
+  render() {
 
-    render(){
+    return (
+      <Query query={USER_QUERY} variables={{ id: this.props.match.params.id }}>
+        {({ data, loading, error }) => {
+          if (loading) return <div> Loading..</div>;
+          if (error) return <div> Something went wrong...</div>;
+          console.log("Profile:",{ data });
 
-        return(
-            <div>Profile</div>
-        )
-    }
+          return <div>Profile for: {data.user.username} </div>;
+        }}
+      </Query>
+    );
+  }
 }
 
-export default Profile
+export default Profile;
+
+const USER_QUERY = gql`
+  query($id: Int!) {
+    user(id: $id) {
+      username
+      postSet {
+        image
+        caption
+        comments {
+          content
+          user {
+            username
+          }
+        }
+      }
+    }
+  }
+`;
