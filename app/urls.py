35dbb3ccef7158f2dfd5219624b7f7fcfb8bbf6a
Arnold Kibira <arnold.ril3y@gmail.com>
2020-03-31 22:40:21 +0300

    update settings

--- app/urls.py
+++ app/urls.py
@@ -19,13 +19,11 @@ from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.conf.urls.static import static
-from insta.views import FrontendAppView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    re_path(r'^', FrontendAppView.as_view())
 ]
 
 if settings.DEBUG:
